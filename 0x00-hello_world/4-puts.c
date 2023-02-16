f9b146bfdf54d6277c3ecc682c42837f6b0a59cc
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-16 12:11:03 -0800

    puts C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/*
-*main - Entry point
-*Description: prints "Programming is like building a multilingual puzzle"
-*Return: Always 0 (success)
-*/
-"\n" int main(void)
+
+/**
+ *main - Entry point
+ *
+ *Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *Return: Always 0 (success)
+ */
+int main (void)
 {
-"\t" puts("Programming is like building a multilingual puzzle\n");
-"\t" return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
 }
+	

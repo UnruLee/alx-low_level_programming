0ef748b08db8356f42ace94a72d556fb5221d276
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-16 14:35:47 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", (int)
 	sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	
+
 	return (0);
 }

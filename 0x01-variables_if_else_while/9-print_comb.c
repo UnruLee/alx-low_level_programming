14e93de8232a3b8e5a75be7b398a474767619412
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-17 09:04:36 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible combination of single digit numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+
+	putchar('\n')
+
+	return (0);
+}

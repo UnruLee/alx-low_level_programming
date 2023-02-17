058c127d917b405990c8f6be7aea5af166b42804
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-17 08:28:30 -0800

    putchar digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints single digit numbers using putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a);
+		a++
+	}
+
+	putchar('\n');
+
+	return (0);
+}

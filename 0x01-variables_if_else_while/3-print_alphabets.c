17146990116e954ec22403676d84ab0317c78d5d
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-17 04:40:00 -0800

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints lowercase and uppercase of alphabets
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

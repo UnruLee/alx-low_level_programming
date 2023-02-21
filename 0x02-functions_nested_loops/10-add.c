8a18790a900339a9cfe53a571c4e3e0cb20b67dd
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 15:57:47 -0800

    add integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: a is an ascii characters
+ * @b: b is an ascii character
+ *
+ * Description: adds two integers
+ *
+ * Return: 0 (success)
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}

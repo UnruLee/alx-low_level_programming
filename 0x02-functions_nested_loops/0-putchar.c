bc374308cb84b2541a32c27f028dba5ad84ff287
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 07:27:22 -0800

    Alphabet check

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _putchar - Entry point
+ * @a: a is an ascii character
  *
  * Description: print out '_putchar'
  *
  * Return: 0 (success)
  */
 
-int main(void)
+int _putchar(char a)
 {
+	char a = '_putchar';
 	_putchar("_putchar\n" + '0');
 
 	return (0);
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: c is an ascii character
+ *
+ * Description: check if value is an alphabet
+ *
+ * Return: 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}

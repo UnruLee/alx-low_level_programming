9d8f009472b6d50d245af6bd1939d515526fcbbf
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 15:31:21 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: n is a a variable for int
+ *
+ * Description: print last digit
+ *
+ * Return: 0 (success)
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	while (n > 0)
+		_putchar(last_digit + '0');
+
+	return (0);
+}

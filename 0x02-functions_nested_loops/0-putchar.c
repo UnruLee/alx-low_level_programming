fdee02cf3ce217ab8d87e9997abb950c763bfc38
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 05:21:44 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print out '_putchar'
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar("_putchar\n");
+
+	return (0);
+}

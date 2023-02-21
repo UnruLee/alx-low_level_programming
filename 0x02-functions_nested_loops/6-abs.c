d80a37c8de2bbc1809b1dc21c190e9286664703c
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 15:14:47 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @int: int a data type
+ *
+ * Description: prints absolute value
+ *
+ * Return: 0 (success)
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+	else
+		return (n);
+}

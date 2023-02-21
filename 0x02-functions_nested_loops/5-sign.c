f3c222831e6f5ed1b39d01f34463973087f9bb17
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-21 15:02:27 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: n is an ascii character
+ *
+ * Description: prints the sign of a number
+ *
+ * Return: 0 (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	else if (n == o)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return ('-');
+	}
+}

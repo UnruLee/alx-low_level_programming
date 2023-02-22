6d797c809e9c711da4b04ed778c1fa9ef2f1eaf5
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-22 08:12:27 -0800

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,22 +13,18 @@ int main(void)
 	long int first = 0;
 	long int second = 1;
 	long int next;
-	
 	int count;
-	
+
 	while (count < 97)
 	{
 		next = first + second;
 		printf("%lu, ", next);
-		
 		first = second;
 		second = next;
-		
 		count++;
 	}
-	
 	next = first + second;
 	printf("%lu\n", next);
-	
+
 	return (0);
 }

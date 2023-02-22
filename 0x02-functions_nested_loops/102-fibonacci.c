5382a13d744ae40d0f4e5f052b8afc60a685879f
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-22 06:50:44 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.c>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the first fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	long int first = 1;
+	long int second = 2;
+	long int next;
+
+	while (count <= 50)
+	{
+		next = first + second;
+		printf("lu, ", next);
+
+		first = second;
+		second = next;
+
+		count++;
+	}
+
+	next = first + second;
+	printf("lu\n", next);
+
+	return (0);
+}

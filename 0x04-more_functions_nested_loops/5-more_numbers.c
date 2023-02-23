b60c6ed895a1b3195c7c4ed92e40f01031ae685e
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-23 05:17:14 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: prints 10 times the number from 0 to 14
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i = o;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar("\n");
+	}
+}

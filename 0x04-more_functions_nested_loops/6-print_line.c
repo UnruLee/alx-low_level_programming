8562a889982b59cf19d16ccfe929cd3b3e0a7c60
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-23 15:25:16 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Entry poimt
+ * @n: ascii character to hold variable
+ *
+ * Description: function that draws a straight linw in the terminal
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int line = 0;
+
+	while (line < 0;)
+	{
+		_putchar('_');
+		line++;
+	}
+	_putchar("\n");
+}

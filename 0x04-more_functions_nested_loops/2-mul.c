e05c27741396dfaefd3c8c988003cd882f45f65b
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-23 04:31:16 -0800

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: ascii character for variable
+ * @b: ascii character for variable
+ *
+ * Description: multiply two integers
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	_putchar(prod);
+
+	return (0);
+}

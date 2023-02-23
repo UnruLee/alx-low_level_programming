32e07838a61265faac689e4bf4057df7e167f2d0
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-23 05:03:34 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: print numbers 0-9 except 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n);
+		}
+		_putchar("\n");
+	}
+}

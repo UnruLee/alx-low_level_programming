ec3328e3230da3a006b349893f9a678fe0183fb0
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 05:20:30 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n < '10'; n++)
 	{
-		if (n != 2 && n != 4)
+		if (n != '2' && n != '4')
 		{
 			_putchar(n);
 		}

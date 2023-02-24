027c29c3331bbb813cda1283345ca557cb30e46c
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 08:48:29 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_line - Entry poimt
+ * print_line - Entry point
  * @n: ascii character to hold variable
  *
- * Description: function that draws a straight linw in the terminal
+ * Description: A function that draws a straight line in the terminal
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void print_line(int n)
 {
 	int line = 0;
 
-	while (line < 0;)
+	while (line < n)
 	{
 		_putchar('_');
 		line++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }

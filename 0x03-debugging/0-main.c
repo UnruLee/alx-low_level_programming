8a526e891ec7a2161cf9d278d9ccd614d4018fab
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-23 23:20:17 -0800

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: test positive or negative with 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

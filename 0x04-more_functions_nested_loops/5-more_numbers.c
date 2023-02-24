469e80d0831d37babf8b2560abcaa276a07b39f7
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 05:38:04 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,11 @@ void more_numbers(void)
 	int i = o;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j);
-			j++;
 		}
 		_putchar("\n");
 	}

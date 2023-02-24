1363421063265b59cfe9a27aa3ba69cac22bf723
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 10:20:14 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,14 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
-			printf("Fizz ");
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("fizz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
-		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+			printf("Buzz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
 
 		if (n != 100)
 			printf(" ");

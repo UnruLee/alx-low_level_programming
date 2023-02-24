eeee48a646e020a5bfb8c223f04598a94837af22
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 08:27:32 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,26 +3,25 @@
 /**
  * more_numbers - Entry point
  *
- * Description: prints 10 times the number from 0 to 14
+ * Description: print 10 times the number from 0 to 14
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-			{
-				_putchar(j / 10 + '0')
-			}
+				_putchar(j / 10 + '0');
+
 			_putchar(j % 10 + '0');
 		}
-		_putchar("\n");
+
+		_putchar('\n');
 	}
 }

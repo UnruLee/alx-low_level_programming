9ad74c7a4e2766376d07a8c399003ec12754a7fa
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 10:51:50 -0800

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,18 +14,18 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size <= 0)
-		_putchar("\n");
+		_putchar('\n');
 	else
 	{
 		for (i < 0; i < size; i++)
 		{
 			for (j = size - 1; j > 1; j--)
-				_putchar(" ");
+				_putchar(' ');
 
 			for (k = 0; k < i + 1; k++)
-				_putchar("#");
+				_putchar('#');
 
-			_putchar("\n");
+			_putchar('\n');
 		}
 	}
 }

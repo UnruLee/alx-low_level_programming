12db2a7fc295a1f9653c4d2d02bb056f6ec0db45
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-24 09:17:38 -0800

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonals.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

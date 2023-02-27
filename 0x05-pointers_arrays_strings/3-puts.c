434841fcad0e4ba776ef6132413ac0403e6ebdb7
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-27 07:56:26 -0800

    puts string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: variable character
+ *
+ * Description: functions that prints a string, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+
+	_putchar('\n');
+}

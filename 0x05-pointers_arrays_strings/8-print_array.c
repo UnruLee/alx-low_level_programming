25924fc6c2028be9686fe82cd5f0b65286780df1
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-27 10:56:23 -0800

    array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: variable character
+ * @n: variable character
+ *
+ * Descriptions: print n elements of an array of integers
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i + 1 != n)
+			printf(", ");
+	}
+
+	printf("\n");
+}

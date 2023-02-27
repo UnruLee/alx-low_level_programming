f291bfa939629bfd22443263ae466c85b4fd04f1
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-27 06:31:00 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: variable to hold pointer
+ *
+ * Description: a function that updates the value of a pointer to 98
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

a76e2d64da2ccd985e933adba4e80bb6f1da78af
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-02-27 09:27:03 -0800

    string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: variable character
+ *
+ * Description: prints every other character of a string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n += 2;
+	}
+
+	_putchar('\n');
+}

6e79592bc7c83bb00cc8debad039cd78bf762476
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-02 02:58:49 -0800

    string n concat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,7 +19,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n && src[i] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}

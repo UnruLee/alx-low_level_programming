adee33dd54ac8f0c465ddbfc59081177d4206fff
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-02 05:23:29 -0800

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *cap_string - Entry point
+ * @str: blank variable
+ *
+ * Description: function that capitalized all words of a string
+ *
+ * Return: Always 0
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0')
+	{
+		j = i - 1;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+				str[i] = str[i] - 32;
+			else if (str[j] >= 9 && str[j] <= 10)
+				str[i] = str[i] - 32;
+			else if (str[j] >= 32 && str[j] <= 34)
+				str[i] = str[i] - 32;
+			else if (str[j] >= 40 && str[j] <= 41)
+				str[i] = str[i] - 32;
+			else if (str[j] == 46)
+				str[i] = str[i] - 32;
+			else if (str[j] == 59)
+				str[i] = str[i] - 32;
+			else if (str[j] == 123 || str[j] == 125)
+				str[i] = str[i] - 32;
+		}
+	}
+
+	return (str);
+}

1f1a7c48e2fcab887c07b78ee9d774f38323804f
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-06 11:15:28 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: variable character
+ * @accept: variable character
+ *
+ * Description: function that gets the length of a prefix substring
+ *
+ * Return: Always 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (count != !)
+			break;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+		}
+	}
+
+	return (count);
+}

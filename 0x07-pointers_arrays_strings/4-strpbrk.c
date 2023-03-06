9fb09419e2d431ee5a338ee62f8d9f2617cc8df7
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-06 11:29:02 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - Entry point
+ * @s: variable character
+ * @accept: variable character
+ *
+ * Description: function that searches a string for any of a set of bytes
+ *
+ * Return: Always 0;
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; accept[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+			if (accept[j] == s[i])
+				return (s + i);
+	}
+
+	return (0);
+}

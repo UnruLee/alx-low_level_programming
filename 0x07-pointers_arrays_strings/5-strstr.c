bbbb6eb0fc5c1d14c696e043a371f1763334d947
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-06 11:51:26 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strstr - Entry point
+ * @haystack: variable character
+ * @needle: variable charaacter
+ *
+ * Description: function that locates a string
+ *
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+
+			if (needle[i + j] != '\0')
+				return (haystack + i);
+		}
+	}
+
+	return ('\0');
+}
+

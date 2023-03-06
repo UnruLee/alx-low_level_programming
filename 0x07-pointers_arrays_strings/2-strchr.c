b5c180fbec9bd89cc6331b7f3a050bd9a438e075
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-06 10:45:28 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strchr - Entry point
+ * @s: : vaariable character
+ * @c: variable character
+ *
+ * Description: function that locates a character in a string
+ *
+ * Return: Always 0
+ */
+
+char *_strchr(char *s, char *c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + 1);
+	}
+
+	if (s[i] == c)
+		return (s + 1);
+	else
+		return ('\0');
+}

aeb6869fa85444de788e1e755e3aff5b1ff879c1
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-07 07:49:15 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Entry point
- * @x: variable character
- * @y: variable character
+ * _pow_recursion - Entry Point
+ * @n: variable character
  *
- * Description: function that returns the value of x raised to y
+ * Description: function that retursn the value of x raised to y
  *
- * Return: Always 0
+ * Return:Always 0
  */
 
-int _pow_recursion(int x, int y)
+int _pow_recursion(int n)
 {
-	if (y < 0)
-		return (-1);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
 	else if (y == 0)
 		return (1);
-	else if (y > 0)
-		return (x * _pow_recursion(x, y - 1));
+	else
+		return (-1);
 }

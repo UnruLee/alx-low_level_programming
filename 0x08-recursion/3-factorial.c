64d6e21b96afb0985f6f4199e64271f6d2c68ca7
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-07 06:26:00 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: variable character
+ *
+ * Description: fucntion that prints factorial of a given number
+ *
+ * Return: Always 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (n * factorial(n - 1));
+}

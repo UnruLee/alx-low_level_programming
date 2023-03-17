83c9928912765e37aed3f007800d0d32c352c07e
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-17 01:37:16 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Entry point
+ * @nmeb: variable character
+ * @size: variable character
+ *
+ * Description: function that allocates memory for an array
+ *
+ * Return: returns a pointter to the allocated memory
+ */
+void *_calloc(unsigned int nmeb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmeb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(size * nmeb);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size * nmeb; i++)
+		arr[i] = 0;
+
+	return (arr);
+}

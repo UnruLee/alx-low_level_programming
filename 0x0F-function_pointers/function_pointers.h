8b7fbad754bc8b137f92caa03f4c9224b0bb579e
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-22 11:34:58 -0700

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_POINTERS_N
 #define FUNCTION_POINTERS_N
 
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef __SIZE_TYPE__ size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

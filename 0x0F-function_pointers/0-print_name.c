19b3adc53e271a0fcc0f747c946475d67b59a81b
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-22 07:37:16 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ * @name: variable for name
+ * @f: variable for function pointer
+ *
+ * Description: function that prints a name
+ *
+ * Return: Success
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == 0)
+		return;
+	f(name);
+}

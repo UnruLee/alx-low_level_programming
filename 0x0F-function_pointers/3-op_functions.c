423a5877a78108a2763af33baac55b6f71a49fcc
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-22 14:24:23 -0700

    main

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - entry function
+ * @a: variable for first number
+ * @b: variable for second number
+ *
+ * Description: function that adds two numbers
+ *
+ * Return: Sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry function
+ * @a: variable for first number
+ * @b: variable for second number
+ *
+ * Description: function that subtracts two numbers
+ *
+ * Return: Subtraction of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b)
+}
+
+/**
+ * op_mul - entry function
+ * @a: variable for first number
+ * @b: variable for second number
+ *
+ * Description: function that multiplies two numbers
+ *
+ * Return: multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b)
+}
+
+/**
+ * op_div - entry function
+ * @a: variable for first number
+ * @b: variable for second number
+ *
+ * Description: function that divides two numbers
+ *
+ * Return: division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+
+/**
+ * op_add - entry function
+ * @a: variable for first number
+ * @b: variable for second number
+ *
+ * Description: function that finds modulus of two numbers
+ *
+ * Return: modulus of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b)
+}

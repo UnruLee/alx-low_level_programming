2626ed5da9766da956521e83b71b1c4d20388d0f
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-22 08:04:19 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL && size >= 1 action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)

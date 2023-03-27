cb3f75a9fa74f001ee4a51a83792e4549ee6533a
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-26 22:23:45 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,71 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to copy
+ *
+ * Description: returns a pointer to a newly allocated space in memory
+ *
+ * Return: Success
+ */
+char *_strdup(char *str)
+{
+	int n, i;
+	char *str;
+
+	if (str == NULL)
+		return (0);
+
+	n = 0;
+	while (*(s + n))
+		n++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = *(str + i);
+	}
+	return (s);
+}
+
+/**
+ * new_dog - entry point
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: creates a new dog
+ *
+ * Return: On success 1.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == 0 || name == 0 || owner == 0)
+		return (0);
+
+	new_dog->name = _strdup(name);
+	if (new_dog->name == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == 0)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (0);
+	}
+	return (new_dog);
+}

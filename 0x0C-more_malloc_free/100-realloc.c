f7ec00e4ef1f523c2a5ea6acde9b042d050e0d31
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-03-26 23:01:45 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,65 +1,35 @@
-#include "mainn.h"
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * _memcpy - entry point
- * @dest: destination
- * @src: source
- * @n: size of memory to copy
- *
- * Description: copies memory
- *
- * Return: Success
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	return (dest);
-}
-
-/**
  * _realloc - entry point
- * @ptr: pointer to modify
- * @old_size: current size of memory
- * @new_size: size memory will now have
+ * @ptr: pointer
+ * @old_size: byte size to be allocated
+ * @new_size: new size in bytes
  *
- * Description: reallocated a memory block using malloc and free
- *
- * Return: Pointer to reallocated memory
+ * Description: reallocates memory block using malloc and free
+ * Return: void
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *t;
-
-	if (old_size == new_size)
-		return (ptr);
-
 	if (ptr == NULL)
 	{
-		t = malloc(new_size);
-		if (t == 0)
-			return (0);
-		free(ptr);
-		return (t);
+		ptr = malloc(new_size);
+		return (ptr);
 	}
 
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
 
-	t = malloc(new_size);
-	if (t == 0)
-		return (0);
-
-	_memcpy(t, ptr, old_size);
+	if (new_size == old_size)
+		return (ptr);
 	free(ptr);
-	return (t);
+
+	ptr = malloc(new_size);
+
+	return (ptr);
 }

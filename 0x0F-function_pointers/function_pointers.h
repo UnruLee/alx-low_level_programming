b88a73022f08fb52c35c334ea2108cdc913a31e6
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-04-22 21:57:35 +0100

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_POINTERS_N
-#define FUNCTION_POINTERS_N
-
-typedef __SIZE_TYPE__ size_t;
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif

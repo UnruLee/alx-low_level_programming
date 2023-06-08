150a5c63cdcc2778b27d24f254d2d92effa1ce4d
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-08 03:37:55 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - entry point
+ * @s1: function parameter one
+ * @s2: function parameter two
+ *
+ * Description: a function that compares two strings and returns 1
+ * if the strings can be considered identical, otherwise return 0.
+ *
+ * Return: Always 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s1 != '\0')
+		return (0);
+
+	return (wildcmp(s1 + 1, s2 + 1));
+}

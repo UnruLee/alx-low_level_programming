3c6f33b6f0d95e01479a56173d691341d66c37e2
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-09 07:05:01 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: a count of the arguments supplied to the program
+ * @argv: an array of pointers to the strings supplied to the program
+ *
+ * Description: a program that prints the number of arguments passed into it.
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

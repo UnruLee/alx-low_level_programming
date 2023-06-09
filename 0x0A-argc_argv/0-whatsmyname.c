40e27f74ae8ea8c6057bd428148989694b66dd2f
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-09 06:29:00 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: a count of the argument supplied to the program
+ * @argv: an array of pointers to the strings supplied in the program
+ *
+ * Description: a program that prints its name
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

3a5d545d391d9e2a8fa7907b1463525bdd8c504f
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-13 15:59:40 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,10 +20,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s = (char*)malloc(sizeof(char) * size);
+	s = (char *) malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
-		s[i] = c;
+		*(s + i) = c;
 
 	return (s);
 }

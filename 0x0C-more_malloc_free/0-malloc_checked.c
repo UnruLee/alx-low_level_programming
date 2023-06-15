3e69aec3186a135a2655f9b2ea0b2b34140643bf
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-15 14:51:23 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Entry point
+ * @b: is a variable character
+ *
+ * Description: a function that allocates memory using malloc
+ *
+ * Return: 0 (success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (b);
+}

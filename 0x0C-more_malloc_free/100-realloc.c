4ed3c2a3834d451f1e3d53818167d67d92e38d19
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-16 16:10:44 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - Entry point
+ * @new_size: is a variable character
+ * @old_size: is a variable character
+ * @ptr: is a variable character
+ *
+ * Description:  function that reallocates a memory block using malloc and free
+ *
+ * Return: 0 (success)
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (p);
+
+	if (p == NULL)
+	{
+		p = malloc(new_size);
+
+		if (p == NULL)
+			return (NULL);
+
+		return (p);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(p);
+			return (NULL);
+		}
+	}
+
+	p malloc(new_size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		p[i] = ((char *) p)[i];
+
+	free(p);
+	return (p);
+}

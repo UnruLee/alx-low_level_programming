d2fcb22e24e505d0e9377146a8e3d9d6eb9bdea3
Jackson Ezekiel <jackson.ezekiel.st@gmail.com>
2023-06-19 15:13:06 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,11 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %s\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: %s\n", d->owner);
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %s\n", d->age);
+		
+		if (d->owner == NULL)
+			printf("Owner: %s\n", d->owner);
 	}
 }
